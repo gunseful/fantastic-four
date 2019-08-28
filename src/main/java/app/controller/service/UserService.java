f9886b5cc52c06ad94d5263e8c28d3aed8b7d251
gunseful <gunseful@gmail.com>
2019-08-28 23:18:36 +0600

    beta v 5.3.2 app.model.dao + service + lambda
    Signed-off-by: gunseful <gunseful@gmail.com>

--- src/main/java/app/controller/service/UserService.java
+++ src/main/java/app/controller/service/UserService.java
@@ -6,21 +6,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface UserService {
-    public List<User> getBlackList() throws SQLException;
+    List<User> getBlackList() throws SQLException;
 
-    public boolean addNewUser(User user) throws SQLException;
+    boolean addNewUser(User user) throws SQLException;
 
-    public boolean checkLogginAndPassword(User user) throws SQLException;
+    boolean checkLogginAndPassword(User user) throws SQLException;
 
-    public User getUser(int id) throws SQLException;
+    User getUser(int id) throws SQLException;
 
-    public User getUserByNickname(String nickname) throws SQLException;
+    User getUserByNickname(String nickname) throws SQLException;
 
-    public boolean checkBlackList(User user) throws SQLException;
+    boolean checkBlackList(User user) throws SQLException;
 
-    public void addToBlackList(int id) throws SQLException;
+    void addToBlackList(int id) throws SQLException;
 
-    public void removeFromBlackList(int id) throws SQLException;
+    void removeFromBlackList(int id) throws SQLException;
 
 
 }
